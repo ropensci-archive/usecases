e3f8dfa6ed9803d3fbd9889e09b1b5d67e105e45
Scott Chamberlain <myrmecocystus@gmail.com>
2011-12-13 14:33:28 -0600

    added rplos folder and edited readme

---
+++ rplos/R/rplos_eg.R
@@ -0,0 +1,31 @@
+##############################################################
+# rplos use-case: "What drives PLoS ONE's high impact factor?"
+##############################################################
+# Install rplos and other packages
+require(devtools); require(ggplot2)
+install_github("rplos", "ropensci")
+require(rplos)
+
+# citation distribution
+  # Get DOIs for all PLoS One articles
+
+
+  # Get total citation from all sources for each DOI
+almtotcites()
+
+
+# keywords in the most cited articles
+
+
+
+# growth of plos one
+
+
+
+# growth related to other plos journals
+
+
+
+# alt-metrics about plos one articles
+
+
