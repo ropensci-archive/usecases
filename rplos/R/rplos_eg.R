4e925e74a8d7215070b94635448d075976eee6a3
Scott Chamberlain <myrmecocystus@gmail.com>
2011-12-14 23:43:51 -0600

    The rplos use case now: gets some DOIs, then gets ALM total cites in a vector

--- rplos/R/rplos_eg.R
+++ rplos/R/rplos_eg.R
@@ -2,16 +2,31 @@
 # rplos use-case: "What drives PLoS ONE's high impact factor?"
 ##############################################################
 # Install rplos and other packages
-require(devtools); require(ggplot2)
+require(devtools)
 install_github("rplos", "ropensci")
 require(rplos)
 
-# citation distribution
-  # Get DOIs for all PLoS One articles
-searchplos('the', 'id', 1000)
+# Citation distribution
+GetAllDOIs <- function(search) {   # Get DOIs for all PLoS One articles
+  searchplosfx <- function(x, y){
+    searchplos(terms=x, 
+              fields='id', 
+              toquery='doc_type:full', 
+              start=y, 
+              limit=250)[[2]]}
+  llply(seq(0,30000,250), function(t) 
+    searchplosfx(search, t), .progress='text')
+}
+results <- GetAllDOIs('*:*') # get all DOIs
+results_ <- as.vector(sapply(results, function(x) x[[1]], simplify=T)) # to vector
+
+  # Get article DOIs only (i.e., remove image DOIs)
+DO THIS BEFORE NEXT STEP
 
   # Get total citation from all sources for each DOI
-almtotcites()
+almtotcites(str_trim(as.character(results[[1]][1,1]), "both"))
+almtotcites(str_trim(results_[1], "both"))
+citesout <- laply(results_[1:15], function(x) almtotcites(str_trim(x,'both')), .progress='text')
 
 
 # keywords in the most cited articles
