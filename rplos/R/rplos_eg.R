a4310b5e44b7ca098a05b93da3548f050e7132c0
Scott Chamberlain <myrmecocystus@gmail.com>
2011-12-15 12:30:41 -0600

    updated rplos use case for plos one question that Carl posed

--- rplos/R/rplos_eg.R
+++ rplos/R/rplos_eg.R
@@ -9,12 +9,17 @@ require(rplos)
 # Citation distribution
 GetAllDOIs <- function(search) {   # Get DOIs for all PLoS One articles
   searchplosfx <- function(x, y){
+#     searchplos(terms=x, 
+#               fields='id', 
+#               toquery='doc_type:full', 
+#               start=y, 
+#               limit=250)[[2]]
     searchplos(terms=x, 
               fields='id', 
-              toquery='doc_type:full', 
+              toquery=list('cross_published_journal_key:PLoSONE', 'doc_type:full'), 
               start=y, 
               limit=250)[[2]]}
-  llply(seq(0,30000,250), function(t) 
+  llply(seq(0,1250,250), function(t) 
     searchplosfx(search, t), .progress='text')
 }
 results <- GetAllDOIs('*:*') # get all DOIs
@@ -24,15 +29,21 @@ results_ <- as.vector(sapply(results, function(x) x[[1]], simplify=T)) # to vect
 results_trim <- sapply(results_, str_trim, side='both', USE.NAMES=F) # trim whitespace
 
   # Get total citation from all sources for each DOI
-crossrefcites <- laply(results_trim[1:15], function(x) 
+crossrefcites <- laply(results_trim, function(x) 
   almplosallviews(x, 'crossref', F, F, 'json')$article$citations_count, .progress='text')
+
+  # plot distribution
+dat <- as.data.frame(crossrefcites)
+ggplot(dat, aes(crossrefcites)) +
+  theme_bw(base_size=18) +
+  geom_histogram() +
 
 # keywords in the most cited articles
 
 
 
 # growth of plos one
-
+  # plot number of articles by year
 
 
 # growth related to other plos journals
